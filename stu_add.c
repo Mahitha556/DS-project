e896a96f9a32ca7343e45ece16099e66e530a727
Mahitha556 <mahithainkollu@gmail.com>
2026-09-06 00:18:42 +0530

    Add files via upload

---
+++ stu_add.c
@@ -0,0 +1,27 @@
+#include"header.h"
+stu *head=0;
+void stud_add()
+{
+stu *new,*p=head;
+int t;
+new=malloc(sizeof(stu));
+static int i=1;
+l:printf("enter student name and percentage\n");
+scanf("%s%f",new->name,&new->percentage);
+if(new->percentage<=0 || new->percentage>=100)
+{
+        printf("percentage is out of range\n");
+        goto l;
+}
+new->rollno=i++;
+new->next=0;
+if(head==0)
+        head=new;
+else
+{
+        stu *last=head;
+        while(last->next)
+                last=last->next;
+        last->next=new;
+}
+}
